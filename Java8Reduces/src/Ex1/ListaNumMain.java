2b323994f5f177436dc1d24282cd69ad5565ba0f
Guilherme <santos.g@aluno.ifsp.edu.br>
2021-02-16 12:15:39 -0300

    java8 reduces exercicios

---
+++ Java8Reduces/src/Ex1/ListaNumMain.java
@@ -0,0 +1,14 @@
+package Ex1;
+
+public class ListaNumMain {
+
+	public static void main(String[] args) {
+		
+		ListaNum list = new ListaNum();
+		
+		//list.ListaSomaNumeros();
+		//list.ListaMultiplicaNumeros();
+		list.ListaMenorNumero();
+		//list.ListaMaiorNumero();
+	}
+}
---
+++ Java8Reduces/src/Ex2/ListaStringMain.java
@@ -0,0 +1,13 @@
+package Ex2;
+
+public class ListaStringMain {
+
+	public static void main(String[] args) {
+		
+		ListaString lista = new ListaString();
+		//lista.ListaConcatenaStringP();
+		//lista.ListaConcatenaStringA();
+		lista.ListaConcatenaStringTodos();
+	}
+	
+}
